df06ba0c5fb708f8c43736fbfcecd5ff7da840cd
kianlamer <kianlamer123@outlook.com>
2024-10-24 16:18:41 -0600

    edited stuff

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,48 @@
+class Journal
+{
+    public List<Entry> Entries {get; set; } = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        Entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in Entries)
+        {
+            Console.WriteLine($"Date: {entry.Date}");
+            Console.WriteLine($"Prompt: {entry.Prompt}");
+            Console.WriteLine($"Response: {entry.Response}");
+            Console.WriteLine();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in Entries)
+            {
+                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        Entries.Clear();
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    Entries.Add(new Entry(parts[1], parts[2], parts[2]));
+                }
+            }
+        }
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,69 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-class Entry
-{
-    public string Prompt { get; set; }
-    public string Response { get; set; }
-    public string Date { get; set; }
-
-    public Entry(string prompt, string response, string date)
-    {
-        Prompt = prompt;
-        Response = response;
-        Date = date;
-    }
-}
-
-class Journal
-{
-    public List<Entry> Entries {get; set; } = new List<Entry>();
-
-    public void AddEntry(Entry entry)
-    {
-        Entries.Add(entry);
-    }
-
-    public void DisplayEntries()
-    {
-        foreach (var entry in Entries)
-        {
-            Console.WriteLine($"Date: {entry.Date}");
-            Console.WriteLine($"Prompt: {entry.Prompt}");
-            Console.WriteLine($"Response: {entry.Response}");
-            Console.WriteLine();
-        }
-    }
-
-    public void SaveToFile(string filename)
-    {
-        using (StreamWriter writer = new StreamWriter(filename))
-        {
-            foreach (var entry in Entries)
-            {
-                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
-            }
-        }
-    }
-
-    public void LoadFromFile(string filename)
-    {
-        Entries.Clear();
-        using (StreamReader reader = new StreamReader(filename))
-        {
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                var parts = line.Split('|');
-                if (parts.Length == 3)
-                {
-                    Entries.Add(new Entry(parts[1], parts[2], parts[2]));
-                }
-            }
-        }
-    }
-}
-
 class Program
 {
     static List<string> prompts = new List<string>
